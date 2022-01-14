21562b87836e2e991e7e341dfbcd6f7f7307ea28
nattakit <search35453@gmail.com>
2022-01-14 18:03:25 +0700

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Questions from './Pages/Questions';
+import Navigation from './Components/Navigation/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Questions" element={<Questions/>}/>
+      </Routes>
       <h1>Hello World</h1>
     </div>
   );
